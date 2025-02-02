b2d6fed1278192ed98af7fa6c68aa8e8a3eb3db9
iceman30386 <bfg33@msn.com>
2025-02-01 19:26:02 -0700

    Menu UI Setup

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.IO;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+    public string sPlayerH;
+    public string sPlayerC;
+    public int iHighScore;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadData();
+
+    }
+    public void SaveData()
+    {
+        SaveData data = new SaveData();
+        data.sPlayerHighScore = sPlayerH;
+        data.iHighScore = iHighScore;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    public void LoadData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            sPlayerH = data.sPlayerHighScore;
+            iHighScore = data.iHighScore;
+        }
+    }
+
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public string sPlayerHighScore;
+    public int iHighScore;
+}
